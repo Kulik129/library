494f4eafbdb64cd01720716302cb72450a3c52cf
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-03-22 23:36:38 +0300

    Модель выдачи

---
+++ issue-service/src/main/java/com/example/models/Issue.java
@@ -0,0 +1,27 @@
+package com.example.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "issue")
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private UUID uuid;
+    @Column(name = "issued_at")
+    private LocalDate issuedAt;
+    @Column(name = "book_id")
+    private UUID bookId;
+    @Column(name = "reader_id")
+    private UUID readerId;
+}
