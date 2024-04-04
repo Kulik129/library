db7d2a89bd11966a115e008665f9d40412317d1a
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-04-04 18:59:25 +0300

    сущность книги

---
+++ consumer/src/main/java/com/example/entity/BookEntity.java
@@ -0,0 +1,36 @@
+package com.example.entity;
+
+import com.example.models.Book;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "book_table")
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+    private String id;
+    private String title;
+    @ElementCollection
+    private List<String> authors;
+    private String publishedDate;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String imageLinks;
+    private String buyLink;
+    @Embedded
+    private Book.ListPrice listPrice;
+    private String previewLink;
+    @OneToMany(mappedBy = "book")
+    private List<IndustryIdentifier> industryIdentifiers;
+    private int pageCount;
+}
+
