1fa1cc5d4ae5752182f2aeb62a4cc8ab5811f77d
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-04-11 17:39:24 +0300

    google books

---
+++ google-books-service/src/main/java/com/example/GoogleBooks.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleBooks {
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleBooks.class, args);
+    }
+}
