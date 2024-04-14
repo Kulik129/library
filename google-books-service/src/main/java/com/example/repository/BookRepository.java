08b2840ce644f398939752e4744fbad31b4c931d
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2024-04-14 21:29:41 +0300

    Доработка

--- google-books-service/src/main/java/com/example/repository/BookRepository.java
+++ google-books-service/src/main/java/com/example/repository/BookRepository.java
@@ -4,6 +4,7 @@ import com.example.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,5 +14,6 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
     @Query("SELECT b FROM BookEntity b WHERE b.title ILIKE %:title%")
     List<BookEntity> findByBookTitle(String title);
 
-    void deleteById(String id);
+    @Transactional
+    void deleteByBookId(Long id);
 }
